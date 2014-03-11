cf65ba765894908c91926cd553a01cb7196b7e3c
MaximeRDY <maxime.raverdy@gmail.com>
2014-03-11 16:49:12 +0100

    Try to fix bug in #namespace_and_name

--- lib/lolsoap/wsdl_parser.rb
+++ lib/lolsoap/wsdl_parser.rb
@@ -214,13 +214,20 @@ module LolSoap
     end
 
     def namespace_and_name(node, prefixed_name, default_namespace = nil)
-      if prefixed_name.include? ':'
-        prefix, name = prefixed_name.split(':')
+      if prefixed_name.nil?
+        prefix = node.namespace.prefix
         namespace = node.namespaces.fetch("xmlns:#{prefix}")
+        name = node.name
       else
-        name      = prefixed_name
-        namespace = default_namespace
+         if prefixed_name.include? ':'
+          prefix, name = prefixed_name.split(':')
+          namespace = node.namespaces.fetch("xmlns:#{prefix}")
+        else
+          name      = prefixed_name
+          namespace = default_namespace
+        end
       end
+
 
       [namespace, name]
     end
